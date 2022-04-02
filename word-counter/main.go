b2437026b903464ec6cc6fb0c4eb192c86117e8b
jay <dj.min43@gmail.com>
2022-04-02 20:47:11 +0900

    add flag to cli command

--- word-counter/main.go
+++ word-counter/main.go
@@ -4,31 +4,37 @@ package main
 // fmt to print "formatted" output
 // io to provide io.Reader interface
 // os to use operating system resources
+// flag package helps manage command line flags such as "-l"
+
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main () {
-	fmt.Println(count(os.Stdin))
+	// this defines a boolean flag -l to count lines instead of words
+	// default value is false for this case
+	lines := flag.Bool("l", false, "Count lines")
+	
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count (r io.Reader) int {
+func count (r io.Reader, countLines bool) int {
 	// this scanner variable "reads" text from a Reader
 	scanner := bufio.NewScanner(r)
-
 	// define the scanner split type to "words"
-	scanner.Split(bufio.ScanWords)
-
+	// if countline is false, this is what we want to do
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 	// this defines word count
 	wc := 0
-
 	for scanner.Scan() {
 		wc++
 	}
-
 	return wc
 }
 
--- word-counter/main_test.go
+++ word-counter/main_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestCountWords(t *testing.T) {
- b := bytes.NewBufferString("word 1 word2 word3 word4\n")
+ b := bytes.NewBufferString("word 1 word2 word3\nline2\nline3 word1")
 
- exp := 4
+ exp := 3
 
- res := count(b)
+ res := count(b, true)
  
  if res != exp {
 	 t.Errorf("Expected %d, got %d intead. \n", exp, res)
